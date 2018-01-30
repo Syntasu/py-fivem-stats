085cc33b9524148cbedbb04a9ef539beea8fc3f2
Syntasu <abouma18@hotmail.com>
2018-01-30 15:42:50 +0100

    JSON parsing on fetch

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
+import json
 from urllib.request import Request, urlopen
+
 
 class ServerListApi:
 
@@ -7,7 +9,7 @@ class ServerListApi:
     def Fetch(self):
         request = Request(self.ServerURL)
         request.add_header('User-Agent', "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46")
-
         response = urlopen(request).read()
+        data = json.loads(response)
 
-        print(response);
+        return data;
