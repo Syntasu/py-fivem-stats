6da4b9131a4b09ee23c9dd3773482f25ce0f8dd5
Syntasu <abouma18@hotmail.com>
2018-01-31 17:07:30 +0100

    The 5pm check-in commit!

--- saver.py
+++ saver.py
@@ -1,12 +1,23 @@
 import time
+import os
 
 class Saver:
 
     def Save(self, data):
+
+        folder = "snapshots/"
+
+        self.__checkDir(folder)
+        
         try:
             snapShotname = "Snapshot_" + time.strftime("%d-%m-%y_%H-%M-%S") + ".json"
-            snapshot = open(snapShotname, "w")
+            snapshot = open(folder + snapShotname, "w")
             snapshot.write(data)
             snapshot.close()
         except IOError:
             print("could not write log files")
+
+
+    def __checkDir(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
